85a5b62abd2a06f677408078aca1e1238b74b598
AndriiPetrovskyi <andriipetrovskyi.it@gmail.com>
2020-10-28 09:45:27 +0200

    corrected font inclide

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,12 @@
 const headerRef = document.querySelector('header');
+const menuListRef = document.querySelector('.header_menu');
+const burgerMenuRef = document.querySelector('.header_burger-menu');
+
 
 window.addEventListener('scroll', () => {
-    headerRef.classList.toggle('white_bg', window.scrollY > 0);
+    headerRef.classList.toggle('white-bg', window.scrollY > 0);
+});
+
+burgerMenuRef.addEventListener('click', () => {
+    menuListRef.classList.toggle('show-menu');
 })
